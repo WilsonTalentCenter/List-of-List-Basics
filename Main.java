0b36a76ee09ba4f0d223811ad1b9f16cde3ff850
David Baker <dbaker@inghamisd.org>
2022-10-17 06:41:46 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+//Declare a list of lists of Integers called integerList
+//Declare a list of lists of Integers called listOfInts
+//Declare a list of lists of Integers called testScores
+
+//Declare a list of lists of Doubles called dblList
+//Declare a list of lists of Doubles called listOfDoubles
+//Declare a list of lists of Doubles called playerPoints
+
+//Declare a list of lists of Strings called stringList
+//Declare a list of lists of Strings called listOfStrings
+//Declare a list of lists of Strings called studentName
+
+//declare a List of lists of integers called intList
+//declare a List of lists of strings  called strList
+
+///////////////Changes to intList///////////////////////////
+//add 2 new lists of integers to intList
+//add 12 to list 0 of intList
+//add 5  to list 0 of intList
+//add 7  to list 0 of intList
+//add 3  to list 1 of intList
+
+//read the first element (element 0)of list 0 of intList into a new variable called intElement
+//print out intElement
+//read the second element (element 1)of list 0 of intList into intElement
+//print out intElement
+//read the third element (element 2)of list 0 of intList into intElement
+//print out intElement
+//read the first element (element 0)of list 1 of intList into intElement
+//print out intElement
+
+//modify element 0 of list 0 of intList to be 13
+//modify element 0 of list 1 of intList to be 8
+
+//read the first element (element 0)of list 0 of intList into intElement
+//print out intElement
+//read the second element (element 1)of list 0 of intList into intElement
+//print out intElement
+//read the third element (element 2)of list 0 of intList into intElement
+//print out intElement
+//read the first element (element 0)of list 1 of intList into intElement
+//print out intElement
+
+
+///////////////Changes to dblList/////////////////////////////
+//add 2 new lists of doubles to dblList
+//add 12.1 to list 0 of dblList
+//add 5.2  to list 1 of dblList
+//add 7.3  to list 1 of dblList
+
+//read the first element (element 0)of list 0 of dblList into a new variable called dblElement
+//print out dblElement
+//read the first element (element 0)of list 1 of dblList into dblElement
+//print out dblElement
+//read the second element (element 1)of list 1 of dblList into dblElement
+//print out dblElement
+
+//modify element 0 of list 1 of dblList to be 13.1
+//modify element 1 of list 1 of dblList to be 8.2
+
+//read the first element (element 0)of list 0 of dblList into dblElement
+//print out dblElement
+//read the first element (element 0)of list 1 of dblList into dblElement
+//print out dblElement
+//read the second element (element 1)of list 1 of dblList into dblElement
+//print out dblElement
+
+///////////////Changes to strList/////////////////////////////
+//add three lists of strings to strList
+//add "Hi" to list 0 of strList
+//add "How"  to list 1 of strList
+//add "Are"  to list 1 of strList
+//add "You?"  to list 2 of strList
+
+//read the first element (element 0)of list 0 of strList into a new variable called strElement
+//print out strElement
+//read the first element (element 0)of list 1 of strList into strElement
+//print out strElement
+//read the second element (element 1)of list 1 of strList into strElement
+//print out strElement
+//read the first element (element 0)of list 2 of strList into strElement
+//print out strElement
+
+//modify element 0 of list 1 of strList to be "good"
+//modify element 1 of list 1 of strList to be "thanks"
+
+//read the first element (element 0)of list 0 of strList into strElement
+//print out strElement
+//read the first element (element 0)of list 1 of strList into strElement
+//print out strElement
+//read the second element (element 1)of list 1 of strList into strElement
+//print out strElement
+//read the first element (element 0)of list 2 of strList into strElement
+//print out strElement
+	}
+
+}
